7f8c29b0bb8549afa8a8077431584325489c9ede
Caleb Cushing <xenoterracide@gmail.com>
2012-11-09 23:38:45 -0600

    first implementation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

---
+++ lib/Test/Subroutine.pm
@@ -0,0 +1,49 @@
+package Test::Subroutine;
+use 5.006;
+use strict;
+use warnings;
+
+# VERSION
+
+use parent 'Exporter';
+use Scalar::Util 'blessed';
+use Test::Builder;
+use Test::More;
+
+our @EXPORT = ( qw( method_is func_is ) );
+
+my $test = Test::Builder->new;
+
+sub method_is {
+	unshift @_, \&is unless ref $_[0] eq 'CODE';
+	my ( $cmp, $obj, $method, $args, $want, $name ) = @_;
+
+	local $Test::Builder::Level = $Test::Builder::Level + 1;
+
+	my $arg_val_name
+		= ! defined $args          ? 'undef'
+		: ref $args # put value of scalar
+			&& ref $args eq 'ARRAY'
+			&& scalar @$args == 1
+			&& ! ref @{ $args }[0] ? @{ $args }[0]
+		:                            '...'
+		;
+
+	$name ||= blessed( $obj ) . '->' . $method . '( ' . $arg_val_name . ' )';
+
+	my $ret = $cmp->( $obj->$method( @$args ), $want, $name );
+
+	return $ret;
+}
+
+sub func_is {
+	unshift @_, \&is unless ref $_[0] eq 'CODE';
+}
+
+1;
+
+# ABSTRACT: test sugar for methods and functions
+
+=head1 SYNOPSIS
+
+	use Test::SubRoutine;
